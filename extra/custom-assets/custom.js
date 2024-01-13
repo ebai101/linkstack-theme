4105dff8a5dff12748e8a79ab1a2a2f5880c5649
Ethan Bailey <ebailey256@gmail.com>
2024-01-12 20:10:25 -0500

    snapchat to spotify

--- extra/custom-assets/custom.js
+++ extra/custom-assets/custom.js
@@ -6,6 +6,14 @@ redditIcon.classList.add('fa-bandcamp');
 redditButton.ariaLabel = 'Bandcamp';
 redditButton.title = 'Bandcamp';
 
+// convert snapchat icon to spotify
+let snapchatIcon = document.getElementsByClassName('fa-snapchat')[0];
+let snapchatButton = document.querySelector('[title="Snapchat"]');
+snapchatIcon.classList.remove('fa-snapchat');
+snapchatIcon.classList.add('fa-spotify');
+snapchatButton.ariaLabel = 'Spotify';
+snapchatButton.title = 'Spotify';
+
 // youtube video embed hack
 let videoLink = document.evaluate('//a[text()="!video-embed"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 if (videoLink !== null) {
